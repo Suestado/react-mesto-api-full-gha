43f5cee82547ba1728f6aec4501a06fdc760271f
Sergey Kiselev <sk.mails@yandex.ru>
2023-07-14 12:33:07 +0300

    fit: mongod connection path changed 3

--- backend/app.js
+++ backend/app.js
@@ -12,16 +12,7 @@ const { corsOptions } = require('./utils/constants');
 const { PORT = 4000 } = process.env;
 const app = express();
 
-mongooseConnect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverSelectionTimeoutMS: 5000,
-  autoIndex: false, // Don't build indexes
-  maxPoolSize: 10, // Maintain up to 10 socket connections
-  serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
-  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-  family: 4, // Use IPv4, skip trying IPv6
-});
+mongooseConnect('mongodb://127.0.0.1:27017/mestodb');
 mongooseConnection.on('error', (err) => console.log(`Ошибка подключения к базе данных: ${err}`));
 mongooseConnection.once('open', () => console.log('Подключение к базе данных установлено'));
 
